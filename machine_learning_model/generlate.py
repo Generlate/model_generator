9066880bfa8c8abf92f4aea9aa5ecdf4b6f146d7
Austen Cabret <austencabret@gmail.com>
2023-06-22 18:54:36 -0400

    added feed forward neural net

--- machine_learning_model/generlate.py
+++ machine_learning_model/generlate.py
@@ -1,4 +1,5 @@
-# create a machine learning model that trains on the modelnet10 dataset to generate novel 3d models
+# create a machine learning model that trains on the
+# modelnet10 dataset to generate novel 3d models
 
 
 # import libraries (pytorch)
@@ -7,3 +8,53 @@
 # propogate n middle layers
 # converge to final node
 # export 3d model (.off)
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Create the dataset
+X_train = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32)
+y_train = torch.tensor([[0], [1], [1], [0]], dtype=torch.float32)
+
+# Define the model
+
+
+class FeedForwardNet(nn.Module):
+    def __init__(self):
+        super(FeedForwardNet, self).__init__()
+        self.fc1 = nn.Linear(2, 4)
+        self.fc2 = nn.Linear(4, 1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return x
+
+
+model = FeedForwardNet()
+
+# Define the loss function and optimizer
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+# Train the model
+num_epochs = 1000
+for epoch in range(num_epochs):
+    optimizer.zero_grad()
+    outputs = model(X_train)
+    loss = criterion(outputs, y_train)
+    loss.backward()
+    optimizer.step()
+
+    if (epoch + 1) % 100 == 0:
+        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
+
+# Evaluate the model
+X_test = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32)
+y_pred = model(X_test)
+print(y_pred)
