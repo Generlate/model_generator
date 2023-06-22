331afc0382cc9ef53997481d78a040d8fc3ad6f2
Austen Cabret <austencabret@gmail.com>
2023-06-22 17:10:02 -0400

    add python file

---
+++ machine_learning_model/generlate.py
@@ -0,0 +1,9 @@
+# create a machine learning model that trains on the modelnet10 dataset to generate novel 3d models
+
+
+# import libraries (pytorch)
+# import data
+# initialize feed forward architecture/weights
+# propogate n middle layers
+# converge to final node
+# export 3d model (.off)
