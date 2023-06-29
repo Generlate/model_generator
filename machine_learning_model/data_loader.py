0a6eebdd62a8513c3954ae40daf84e55f124dbc6
Austen Cabret <austencabret@gmail.com>
2023-06-29 17:42:53 -0400

    updated data loader

--- machine_learning_model/data_loader.py
+++ machine_learning_model/data_loader.py
@@ -1,36 +1,41 @@
 import os
+from torch.utils.data import Dataset
 
-
-class CustomDataset:
+class CustomDataset(Dataset):
     def __init__(self, data_dir):
-        self.data_dir = data_dir
         self.file_list = sorted(os.listdir(data_dir))  # Sort the file list
+        self.file_contents = self.load_file_contents(data_dir)
 
     def __len__(self):
         return len(self.file_list)
 
     def __getitem__(self, idx):
-        file_path = os.path.join(self.data_dir, self.file_list[idx])
-        # Load your .off file and preprocess it if needed
-        # Return the preprocessed data
-        with open(file_path, 'r') as file:
-            data = file.read()
-            print("File:", file_path)
-            print("Content:", data)
-        return data
+        return self.file_contents[idx]
 
+    def load_file_contents(self, data_dir):
+        file_contents = []
+        for file_name in self.file_list:
+            file_path = os.path.join(data_dir, file_name)
+            with open(file_path, 'r') as file:
+                lines = file.readlines()[2:10]  # Read the third through tenth lines
+                content = "".join(lines)
+                formatted_content = self.format_content(content)
+                file_contents.append(formatted_content)
+        return file_contents
 
-data_dir = './Datasets/AustensBoxes/'
+    def format_content(self, content):
+        lines = content.split('\n')
+        formatted_lines = [list(map(float, line.split())) for line in lines if line.strip()]
+        return [val for sublist in formatted_lines for val in sublist]
+
+data_dir = '../Datasets/AustensBoxes/'
 dataset = CustomDataset(data_dir)
-batch_size = 32
 
-num_samples = len(dataset)
-num_batches = (num_samples + batch_size - 1) // batch_size
+# Access file contents from the dataset
+print("Content of the first file:", dataset.file_contents[0])
 
-for batch_idx in range(num_batches):
-    start_idx = batch_idx * batch_size
-    end_idx = min(start_idx + batch_size, num_samples)
-    batch_data = [dataset[idx] for idx in range(start_idx, end_idx)]
+# Get numbers from dataset.file_contents
+number_lists = [[lst[index] for lst in dataset.file_contents if len(lst) > index] for index in range(24)]
 
-    # Your training code here
-    print("Batch Data Length:", len(batch_data))
+for i, numbers in enumerate(number_lists):
+    print(f"{i+1}th numbers:", numbers)
