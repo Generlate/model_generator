f46b0fb8481089a372ac8c46fecb8daf69710346
Austen Cabret <austencabret@gmail.com>
2023-06-29 17:43:23 -0400

    updated the ml model

--- machine_learning_model/example.py
+++ machine_learning_model/example.py
@@ -1,11 +1,90 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
 
-m = torch.nn.Linear(3, 3)
-input = torch.tensor([[1, 2, 3],
-                      [2, 4, 1]], dtype=torch.float32)
-weights = torch.tensor([[1.0, 1.0, 1.0]])
-m.weight = torch.nn.Parameter(weights)
-bias = torch.tensor([[0.0]])
-m.bias = torch.nn.Parameter(bias)
-output = m(input)
-print(output)
+# Define the neural network architecture
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.hidden = nn.Linear(3, 5)  # Hidden layer with 5 units
+        self.output = nn.Linear(5, 1)  # Output layer
+
+    def forward(self, x):
+        x = torch.relu(self.hidden(x))
+        x = self.output(x)
+        return x
+
+# Create an instance of the neural network
+model = NeuralNetwork()
+
+# Define the loss function and optimizer
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Training loop
+training_input = torch.tensor([[-3.29, -3.48, -4.87], [-1.35, -4.01, -0.98], [-1.41, -3.14, -4.72], [3.29, 3.48, 4.87], [-1.35, -4.01, -0.98], [-1.41, -3.14, -4.72], [3.29, 3.48, 4.87], [1.35, 4.01, 0.98],
+                           [-1.41, -3.14, -4.72], [-3.29, -3.48, -4.87], [1.35, 4.01, 0.98], [-1.41, -3.14, -4.72], [-3.29, -3.48, -4.87], [-1.35, -4.01, -0.98], [1.41, 3.14, 4.72], [3.29, 3.48, 4.87], 
+                           [-1.35, -4.01, -0.98], [1.41, 3.14, 4.72], [3.29, 3.48, 4.87], [1.35, 4.01, 0.98], [1.41, 3.14, 4.72], [-3.29, -3.48, -4.87], [1.35, 4.01, 0.98], [1.41, 3.14, 4.72]], dtype=torch.float32)
+target = torch.tensor([[3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3], 
+                        [3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3],
+                        [3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3], [3, 1, 2], [4, 2, 3], ], dtype=torch.float32)
+
+# Test the trained model
+test_input = torch.tensor([[-4.67, -4.91, -1.63], [-1.58, -1.15, -3.94], [-1.02, -3.53, -0.70], [4.67, 4.91, 1.63], [-1.58, -1.15, -3.94], [-1.02, -3.53, -0.70], [4.67, 4.91, 1.63], [1.58, 1.15, 3.94],
+                           [-1.02, -3.53, -0.70], [-4.67, -4.91, -1.63], [1.58, 1.15, 3.94], [-1.02, -3.53, -0.70], [-4.67, -4.91, -1.63], [-1.58, -1.15, -3.94], [1.02, 3.53, 0.70], [4.67, 4.91, 1.63], 
+                           [-1.58, -1.15, -3.94], [1.02, 3.53, 0.70], [4.67, 4.91, 1.63], [1.58, 1.15, 3.94], [1.02, 3.53, 0.70], [-4.67, -4.91, -1.63], [1.58, 1.15, 3.94], [1.02, 3.53, 0.70]], dtype=torch.float32)
+
+num_epochs = 100
+for epoch in range(num_epochs):
+    # Forward pass
+    output = model(training_input)
+    loss = criterion(output, target)
+
+    # Backward pass and optimization
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Print the loss every 100 epochs
+    if (epoch + 1) % 10 == 0:
+        print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+
+
+test_output = model(test_input)
+print("Test Output:")
+print(test_output)
+
+array = test_output.detach().numpy().flatten()
+
+x_list = []
+for x in range(len(array)):
+    if x % 3 == 0:
+        x_list.append(array[x])
+
+y_list = []
+for y in range(len(array)):
+    if y % 3 in [1]:
+        y_list.append(array[y])
+
+z_list = []
+for z in range(len(array)):
+    if z % 3 in [2]:
+        z_list.append(array[z])
+
+# plot the .off
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the 3D coordinates
+ax.scatter(x_list, y_list, z_list)
+
+
+# Set labels for each axis
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Show the plot
+plt.show()
--- machine_learning_model/machine_learning_model_example.py
+++ machine_learning_model/machine_learning_model_example.py
@@ -8,22 +8,16 @@ class SimpleFeedForwardNN(nn.Module):
         super(SimpleFeedForwardNN, self).__init__()
 
         # Manually set the initial weights and biases
-        weight_init = torch.tensor([[0.1, 0.1, 0.1],
-                                    [0.1, 0.1, 0.1],
-                                    [0.1, 0.1, 0.1]])
-        bias_init = torch.tensor([0.0, 0.0, 0.0])
+        weight_init = torch.tensor([[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]])
+        bias_init = torch.tensor([[0.0]])
 
-        self.fullyconnected = nn.Linear(3, 3)  # Input layer: input 3 features, output 3 features.
+        self.fullyconnected = nn.Linear(12, 1)  # Input layer: input 3 features, output 3 features.
         self.fullyconnected.weight = nn.Parameter(weight_init)
         self.fullyconnected.bias = nn.Parameter(bias_init)
 
 
     def forward(self, features):
-        print("features: ", features)
-        print("Weight:", self.fullyconnected.weight)
-        print("Bias:", self.fullyconnected.bias)
         features = (self.fullyconnected(features)) 
-        print("features: ", features)
         return features
 
 
@@ -31,14 +25,13 @@ class SimpleFeedForwardNN(nn.Module):
 model = SimpleFeedForwardNN()
 
 # Create a sample batch of input objects
-input_batch = torch.tensor([[1, 2, 3],
-                            [2, 4, 1]], dtype=torch.float32)
+input_x1 = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]], dtype=torch.float32)
 
 # Pass the input batch through the neural network
-output_object = model(input_batch)
+output_x1 = model(input_x1)
 
 # Print the output object
-print("output batch: ", output_object)
+print(output_x1)
 
 
 
